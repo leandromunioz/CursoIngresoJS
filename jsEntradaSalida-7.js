093f1be4f146dd5dd2ef4298a4f6f772c70f2ccb
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2019-07-09 15:27:48 -0300

    Add files via upload

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,50 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	var num1;
+    var num2;
+    var suma;
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    suma= num1 + num2 ;
+    alert("La suma es " + suma );
+	
+}
+
+function restar()
+{
+    var num1;
+    var num2;
+    var resta;
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    resta= num1 - num2 ;
+    alert("La resta es " + resta );
+}
+
+function multiplicar()
+{ 
+    var num1;
+    var num2;
+    var multiplicacion ;
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    multiplicacion= num1 * num2 ;
+    alert("La multipliacion es " + multiplicacion );
+
+}
+
+function dividir()
+{
+    var num1;
+    var num2;
+    var division;
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    division = num1/num2;
+    alert("La division es " + division);
+    
+}
+
---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function mostrarAumento()
+{
+var sueldo;
+var resultado;
+
+sueldo=parseInt(document.getElementById("sueldo").value);
+resultado= parseInt(document.getElementById("resultado").value)= resultado= sueldo * 0.10;
+
+
+}
